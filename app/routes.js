88458b9ee6e051555ddf6d0944d454ccdcb16a73
Logan Bernard <loganbernard@Logans-Mac-Pro.local>
2014-08-27 14:53:52 -0700

    routes.js

--- app/routes.js
+++ app/routes.js
@@ -3,11 +3,13 @@ var search = require('./models/search');
 module.exports = function(app) {
 
 	// api ---------------------------------------------------------------------
-	
+	// app.get('/recipes', function(req, res) {
+	//  	console.log('req: '+req+' res: '+res)
+	// });
 
 	// application -------------------------------------------------------------
-	app.get('/', function(req, res) {
-		res.sendfile('./public/views/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+	app.get('*', function(req, res) {
+		res.sendfile('./public/views/index.html'); // load the single view file 
 	});
-
+	
 };
